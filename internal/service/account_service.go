9f9a25bd229fd0b9e26fd6250cc1a4e7a70d35e4
yannkistenmacker <yannkistenmacker@gmail.com>
2025-04-08 14:02:23 -0300

    adding func CreateAccount

--- internal/service/account_service.go
+++ internal/service/account_service.go
@@ -1,6 +1,9 @@
 package service
 
-import "github.com/devfullcycle/imersao22/go-gateway/internal/domain"
+import (
+	"github.com/devfullcycle/imersao22/go-gateway/internal/domain"
+	"github.com/devfullcycle/imersao22/go-gateway/internal/dto"
+)
 
 type AccountService struct {
 	repository domain.AccountRepository
@@ -8,4 +11,24 @@ type AccountService struct {
 
 func NewAccountService(repository domain.AccountRepository) *AccountService {
 	return &AccountService{repository: repository}
+}
+
+func (s *AccountService) CreateAccount(input dto.CreateAccountInput) (*dto.AccountOutput, error) {
+	account := dto.ToAccount(input)
+
+	existingAccount, err := s.repository.FindByAPIKey(account.APIKey)
+	if err != nil && err != domain.ErrAccountNotFound {
+		return nil, err
+	}
+	if existingAccount != nil {
+		return nil, domain.ErrDuplicatedAPIkey
+	}
+
+	err = s.repository.Save(account)
+	if err != nil {
+		return nil, err
+	}
+
+	output := dto.FromAccount(account)
+	return &output, nil
 }
