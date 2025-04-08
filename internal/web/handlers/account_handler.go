07939f41dd9c104c0d384ed16fdcac13dd521934
yannkistenmacker <yannkistenmacker@gmail.com>
2025-04-08 15:09:42 -0300

    adding headers and status handler

--- internal/web/handlers/account_handler.go
+++ internal/web/handlers/account_handler.go
@@ -1,6 +1,12 @@
 package handlers
 
-import "github.com/devfullcycle/imersao22/go-gateway/internal/service"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/devfullcycle/imersao22/go-gateway/internal/dto"
+	"github.com/devfullcycle/imersao22/go-gateway/internal/service"
+)
 
 type AccountHandler struct {
 	accountService *service.AccountService
@@ -8,4 +14,21 @@ type AccountHandler struct {
 
 func NewAccountHandler(accountService *service.AccountService) *AccountHandler {
 	return &AccountHandler{accountService: accountService}
+}
+
+func (h *AccountHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var input dto.CreateAccountInput
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	output, err := h.accountService.CreateAccount(input)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(output)
 }
