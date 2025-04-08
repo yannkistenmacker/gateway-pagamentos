f6f23b93b9ae71ce315d2ac828c257b40db51b41
yannkistenmacker <yannkistenmacker@gmail.com>
2025-04-08 18:03:05 -0300

    fix getenv db_port

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -29,7 +29,7 @@ func main() {
 	connStr := fmt.Sprintf(
 		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
 		getEnv("DB_HOST", "db"),
-		getEnv("DB_PORT", "54323"),
+		getEnv("DB_PORT", "5432"),
 		getEnv("DB_USER", "postgres"),
 		getEnv("DB_PASSWORD", "postgres"),
 		getEnv("DB_NAME", "gateway"),
