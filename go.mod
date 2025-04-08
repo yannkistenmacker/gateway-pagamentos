19376344cc58be2dd236a45f46f859eb28da14c2
yannkistenmacker <yannkistenmacker@gmail.com>
2025-04-08 15:34:44 -0300

    fix import

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/devfullcycle/imersao22/go-gateway
 
 go 1.24.2
 
-require github.com/google/uuid v1.6.0
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/google/uuid v1.6.0
+)
--- internal/web/server/server.go
+++ internal/web/server/server.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/devfullcycle/imersao22/go-gateway/internal/service"
 	"github.com/devfullcycle/imersao22/go-gateway/internal/web/handlers"
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5"
 )
 
 type Server struct {
