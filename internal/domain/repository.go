7eea5fe25fb43cf0c8e29887877f30215a7eb8fa
yannkistenmacker <yannkistenmacker@gmail.com>
2025-04-08 14:01:50 -0300

    Fix line 4 adding error

--- internal/domain/repository.go
+++ internal/domain/repository.go
@@ -1,7 +1,7 @@
 package domain
 
 type AccountRepository interface {
-	Save(account *Account)
+	Save(account *Account) error
 	FindByAPIKey(apikey string) (*Account, error)
 	FindByID(id string) (*Account, error)
 	Update(account *Account) error
