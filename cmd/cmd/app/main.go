480415cbeddd5a28fd79362f6b0ade45c26c46b2
yannkistenmacker <yannkistenmacker@gmail.com>
2025-04-08 15:51:06 -0300

    configure main.go

--- cmd/cmd/app/main.go
+++ cmd/cmd/app/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"log"
+	"github.com/joho/godotenv"
+)
+
 func main() {
-	println("Hello World")
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// String connect database
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s"
+		getEnv("DB_HOST", "db"),
+		getEnv("DB_PORT", "54323"),
+		getEnv("DB_USER", "postgres"),
+		getEnv("DB_PASSWORD", "postgres"),
+		getEnv("DB_NAME", "gateway"),
+		getEnv("DB_SSL_MODE", "disable"),
+)
 }
