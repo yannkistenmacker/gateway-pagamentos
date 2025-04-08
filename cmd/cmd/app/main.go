895cde6b1d0b045a57a022a52d793585796842f0
yannkistenmacker <yannkistenmacker@gmail.com>
2025-04-08 17:35:41 -0300

    fix import

--- cmd/cmd/app/main.go
+++ cmd/cmd/app/main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/devfullcycle/imersao22/go-gateway/internal/service"
 	"github.com/devfullcycle/imersao22/go-gateway/internal/web/server"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func getEnv(key, defaultValue string) string {
